415ccc98195c5302813926bdfbbaeeda93b0a42e
Ahmed Salah <salaheltantawy@gmail.com>
2021-01-23 00:58:29 +0200

    frist commet

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import {BrowserRouter as Router} from "react-router-dom"
+
+
+import Hedder from "./components/layout/header"
+
+
+const App = ()=>{
+  return(
+    <Hedder/>
+  )
+}
+
+export default App;
